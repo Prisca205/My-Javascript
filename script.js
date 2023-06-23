fa1229e553309909f611071c2872e66dd0c44035
Prisca205 <dominicakings@gmail.com>
2023-06-23 03:24:32 -0700

    coding challenge

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-console.log('hello')
+/* console.log('hello')
 let ray = 2;
 let laty = 5;
 let rlaty = ray + laty;
-console.log(rlaty);
+console.log(rlaty); */
+
+/*
+java has 8 dart types 
+strings
+numbers
+bigints
+undefined
+null
+symbol
+object
+*/
+
+let read = 'here';
+let work = 'power';
+let readWork = read + work;
+console.log(readWork);
+
+//challenge
+let markmass = 78;
+let markheight = 1.69;
+
+let markTotal  = markmass / markheight ** 2;
+//john
+let johnmass = 92;
+let johnheight = 1.88;
+
+let johnTotal  = markmass / markheight ** 2;
+
+let markHigherBMI = johnTotal > markTotal;
+
+///2nd
+let mark2mass = 95;
+let mark2height = 1.88;
+
+let mark2Total  = markmass / markheight ** 2;
+//john
+let john2mass = 85;
+let john2height = 1.76;
+
+let john2Total  = markmass / markheight ** 2;
+
+let mark2HigherBMI = john2Total > mark2Total;
+console.log(mark2HigherBMI)
+
+
+
+
+
